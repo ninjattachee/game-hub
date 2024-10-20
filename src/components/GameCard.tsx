36e0fd6e6274a933a818f9989401b933ca7c7ac0
atank <atank@atank.org>
2024-10-20 20:53:12 +0800

    Displaying platform icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,7 @@
 
 import { Image, Card, CardBody, Heading } from "@chakra-ui/react"
 import { Game } from "../hooks/useGames"
+import PlatformIconList from "./PlatformIconList"
 
 interface GameCardProps {
     game: Game
@@ -12,6 +13,7 @@ const GameCards = ({ game }: GameCardProps) => {
             <Image src={game.background_image} />
             <CardBody>
                 <Heading fontSize='2xl'>{game.name}</Heading>
+                <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)} />
             </CardBody>
         </Card>
     )
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,6 +1,6 @@
 import { SimpleGrid, Text } from "@chakra-ui/react"
 import useGames from "../hooks/useGames"
-import GameCards from "./GameCards"
+import GameCards from "./GameCard"
 
 
 const GameGrid = () => {
