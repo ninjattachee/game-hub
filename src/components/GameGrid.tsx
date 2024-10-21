1a98e5dd43ad7bb0015de025ebc4b24e202f8984
atank <atank@atank.org>
2024-10-21 20:18:20 +0800

    Refactor GameGrid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -14,14 +14,13 @@ interface GameGridProps {
 const GameGrid = ({ gameQuery }: GameGridProps) => {
     const { datas, error, isLoading } = useGames(gameQuery)
 
+    if (error) return <Text>{error}</Text>
+
     return (
-        <>
-            {error && <Text>{error}</Text>}
-            <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} padding='10px' spacing={6}>
-                {isLoading && Array.from({ length: SkeletonCount }).map((_, index) => <GameCardContainer key={index}><GameCardSkeleton /></GameCardContainer>)}
-                {datas.map(data => <GameCardContainer key={data.id}><GameCards game={data} /></GameCardContainer>)}
-            </SimpleGrid>
-        </>
+        <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} padding='10px' spacing={6}>
+            {isLoading && Array.from({ length: SkeletonCount }).map((_, index) => <GameCardContainer key={index}><GameCardSkeleton /></GameCardContainer>)}
+            {datas.map(data => <GameCardContainer key={data.id}><GameCards game={data} /></GameCardContainer>)}
+        </SimpleGrid>
     )
 }
 
